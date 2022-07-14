8eeb5c441bf777a9104c4204595b257a8cf93f2c
USER-PC\ribeena <ribeena_@hotmail.com>
2022-07-14 18:32:53 +0930

    Add new API which allows other mods to handle the animation and warping.

---
+++ BusLocations/BusStopEventsApi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusLocations.Framework;
+using Microsoft.Xna.Framework;
+
+namespace BusLocations
+{
+    public class BusStopEventsApi
+    {
+        Action jumpToLocation;
+        bool hasJumpToOverride = false;
+        BusLoc goingTo;
+
+        internal void SetGoingTo(BusLoc goTo)
+        {
+            goingTo = goTo;
+        }
+
+        public string GetDestinationMap()
+        {
+            return goingTo.MapName;
+        }
+
+        public Point GetDestinationPoint()
+        {
+            return new Point(goingTo.DestinationX, goingTo.DestinationY);
+        }
+
+        public int GetDestinationFacing()
+        {
+            return goingTo.ArrivalFacing;
+        }
+
+        public void SetJumpToLocation(Action handler)
+        {
+            hasJumpToOverride = true;
+            jumpToLocation = handler;
+        }
+
+        public void JumpToLocation()
+        {
+            jumpToLocation();
+        }
+
+        internal bool HasJumpToOverride()
+        {
+            return hasJumpToOverride;
+        }
+
+    }
+}
