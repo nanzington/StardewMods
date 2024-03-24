37fc0f366295487f21d51bdd3ff50f8db68118a0
Sean Streight <fuzzymage@gmail.com>
2024-03-24 01:16:15 -0400

    BusLocations updated for 1.6

--- LineSprinklers/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("LineSprinklers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
